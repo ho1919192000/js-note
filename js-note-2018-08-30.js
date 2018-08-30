838c12963381d0bc39a11aac92305c7644f4ec61
Howard <ho1919192000@gmail.com>
2018-08-30 17:55:26 -0600

    Add first note

---
+++ js-note-2018-08-30.js
@@ -0,0 +1,35 @@
+/*Basic JavaScript: Find the Length of a String*/
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
+
+/*Basic JavaScript: Use Bracket Notation to Find the First Character in a String*/
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
+
+/*Basic JavaScript: Use Bracket Notation to Find the Nth Character in a String*/
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
+
+/*Basic JavaScript: Use Bracket Notation to Find the Last Character in a String*/
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
+
