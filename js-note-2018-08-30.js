e8913494a30a19a45c4c7a559676ba5a8a8c94eb
Howard <ho1919192000@gmail.com>
2018-09-11 17:54:46 -0600

    Start js Object

--- js-note-2018-08-30.js
+++ js-note-2018-08-30.js
@@ -169,3 +169,86 @@ cc(3);
 cc(7);
 cc('K');
 cc('A');
+
+/*Basic JavaScript: Accessing Object Properties with Variables*/
+
+// Setup
+var testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16; // Change this Line
+var player = testObj[playerNumber]; // Change this Line
+
+/*Basic JavaScript: Delete Properties from a JavaScript Object*/
+
+// Example
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"],
+    "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+
+// Only change code below this line.
+delete myDog["tails"];
+
+/*Basic JavaScript: Using Objects for Lookups*/
+
+// Setup
+function phoneticLookup(val) {
+    var result = "";
+
+    // Only change code below this line
+    /*
+    switch(val) {
+      case "alpha": 
+        result = "Adams";
+        break;
+      case "bravo": 
+        result = "Boston";
+        break;
+      case "charlie": 
+        result = "Chicago";
+        break;
+      case "delta": 
+        result = "Denver";
+        break;
+      case "echo": 
+        result = "Easy";
+        break;
+      case "foxtrot": 
+        result = "Frank";
+    }
+    */
+    var lookup = {
+        "alpha": "Adams",
+        "bravo": "Boston",
+        "charlie": "Chicago",
+        "delta": "Denver",
+        "echo": "Easy",
+        "foxtrot": "Frank"
+    }
+    result = lookup[val]
+    // Only change code above this line
+    return result;
+}
+
+// Change this value to test
+phoneticLookup("charlie");
